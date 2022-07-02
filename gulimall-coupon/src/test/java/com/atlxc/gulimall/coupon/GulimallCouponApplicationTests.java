7098635678289692981c750391971f85465aad94
Kyon-H <a3171218907@qq.com>
2022-07-02 10:30:46 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/atlxc/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlxc.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
